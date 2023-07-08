0e7472126b15bd8a9e4eeb57f8183102b5eb29ee
Vasyl Marusiak <72507878+CartmanUA7@users.noreply.github.com>
2023-07-08 14:10:48 -0400

    Add login screen

---
+++ app/src/main/java/com/vasyl/androidbankingapp/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.vasyl.androidbankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Spinner spinner = findViewById(R.id.spinner_service);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.services_array, R.layout.services_spiner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
