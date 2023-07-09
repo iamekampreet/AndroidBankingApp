2c93871d2cb4c5858714895201fe4fdef5f5b6bb
Vasyl Marusiak <72507878+CartmanUA7@users.noreply.github.com>
2023-07-09 16:10:49 -0400

    Fix login page, add signup page, add initial home page

--- app/src/main/java/com/group2/androidbankingapp/LoginActivity.java
+++ app/src/main/java/com/group2/androidbankingapp/LoginActivity.java
@@ -2,8 +2,10 @@ package com.group2.androidbankingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,5 +20,21 @@ public class LoginActivity extends AppCompatActivity {
                 R.array.services_array, R.layout.services_spiner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button signInButton = (Button) findViewById(R.id.btn_signIn);
+        signInButton.setOnClickListener(v -> signInClick());
+
+        Button registerButton = (Button) findViewById(R.id.btn_register);
+        registerButton.setOnClickListener(v -> registerClick());
+    }
+
+    protected void signInClick(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    protected void registerClick(){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/group2/androidbankingapp/SignupActivity.java
+++ app/src/main/java/com/group2/androidbankingapp/SignupActivity.java
@@ -2,7 +2,9 @@ package com.group2.androidbankingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button registerButton = (Button) findViewById(R.id.btn_register);
+        registerButton.setOnClickListener(v -> registerClick());
+
+        Button backButton = (Button) findViewById(R.id.btn_back);
+        backButton.setOnClickListener(v -> backClick());
+    }
+
+    protected void registerClick(){
+        //validate and register user
+        finish();
+    }
+
+    protected void backClick(){
+        finish();
     }
 }
